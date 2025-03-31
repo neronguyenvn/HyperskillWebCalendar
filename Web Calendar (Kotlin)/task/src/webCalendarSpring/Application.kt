1016ba918561dac05a3adf8d36f69f8a9711c7b2
neronguyenvn <neronguyenvn@gmail.com>
2025-03-31 22:23:00 +0700

    initial commit

---
+++ Web Calendar (Kotlin)/task/src/webCalendarSpring/Application.kt
@@ -0,0 +1,13 @@
+package webCalendarSpring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebCalenderApplication
+
+
+fun main(args: Array<String>) {
+    runApplication<WebCalenderApplication>(*args)
+}
+
